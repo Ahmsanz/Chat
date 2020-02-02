b147e765f7833f071d7c6e2a0b05910f88fd7a71
Ahmsanz <adrian.h.m.sanz@gmail.com>
2020-02-02 20:31:12 +0100

    websockets initialized; connection server-client stablished

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 
+let socket = require('socket.io');
+
+
 //app set up
 
 const app = express();
 
 const server = app.listen(4000, () => console.log('listening to requests on port 4000'));
+
+//static files
+
+app.use(express.static('public'));
+
+//socket set up; fires the method socket and passes the set up server as a parameter.
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('socket connection is made');
+})
